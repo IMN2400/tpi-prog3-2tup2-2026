ae56870d81861630c917cd1cb71cfe611da8ac2d
I M N <imnapal2400@gmail.com>
2026-06-17 17:25:16 -0300

    cambios añadidos en charla con Selene

--- src/components/forumPage/ForumPage.jsx
+++ src/components/forumPage/ForumPage.jsx
@@ -3,8 +3,10 @@ import { useFetchFromAPI } from "../../services/fetch/UseFetchFromAPI"
 
 const ForumPage = ()=>{
     const forumId = useParams();
+    const forum = useFetchFromAPI(`/forum/${id}`)
     const posts = useFetchFromAPI(`/posts/${forumId}`);
     return <>
+    <LesserHeader headerName={forum.nombre} />
         {posts.map((item)=><Post postId={item} />)}
     </>
 }
