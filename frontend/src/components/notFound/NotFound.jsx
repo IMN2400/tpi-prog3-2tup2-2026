456f8a93546a186dc3f728cba739fd6f67f5820e
Leandro-cast <leocastillo2811@gmail.com>
2026-06-22 22:31:44 -0300

    notFound

--- frontend/src/components/notFound/NotFound.jsx
+++ frontend/src/components/notFound/NotFound.jsx
@@ -1,47 +1,51 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
-import {Form, useNavigate } from 'react-router'
+import { Button, ButtonGroup } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom'
 import { useState, useEffect } from 'react';
 import img1 from '../../assets/notFound/img1.jpg';
 import gif1 from '../../assets/notFound/gif1.gif'
-import img2 from '../../assets/notFound/img2.jpg';
-
+import img2 from '../../assets/notFound/img2.jpg'
 const images = [img1, gif1, img2];
 
 const messages = [
-  "Te perdiste 👀",
-  "No hay nada acá...",
-  "404: el vacío te mira de vuelta",
-  "Este no es el foro que buscabas",
-  "Ups... rompiste el internet (no realmente)",
-  "¡Mirá dónde te viniste a meter!",
-  "Te soy sincero... no tengo idea de como hiciste para terminar acá."
+    "Te perdiste 👀",
+    "No hay nada acá...",
+    "404: el vacío te mira de vuelta",
+    "Este no es el foro que buscabas",
+    "Ups... rompiste el internet (no realmente)",
+    "¡Mirá dónde te viniste a meter!",
+    "Te soy sincero... no tengo idea de como hiciste para terminar acá."
 ];
 
 const randomMessage = messages[Math.floor(Math.random() * messages.length)];
 const NotFound = () => {
     const [image, setImage] = useState('');
 
-  useEffect(() => {
-    const randomIndex = Math.floor(Math.random() * images.length);
-    setImage(images[randomIndex]);
-  }, []);
+    useEffect(() => {
+        const randomIndex = Math.floor(Math.random() * images.length);
+        setImage(images[randomIndex]);
+    }, []);
 
     const navigate = useNavigate();
-
-    const handleBackToLogin = () => {
-        navigate("/"); 
-    }
+    const goToLogin = () => { navigate("/login") }
+    const goToMain = () => { navigate("/main") }
 
     return (
         <div>
             <h1>404 - Página no encontrada</h1>
             <p>{randomMessage}</p>
-            <img src={image} alt="random" />
+            <img
+                src={image}
+                alt="random"
+                style={{
+                    width: "900px",
+                    height: "auto"
+                }}
+            />
             <br />
-            <Button onClick={handleBackToLogin}>
-                Volver al inicio de sesión
-            </Button>
+            <ButtonGroup className='my-3 py-5' size='lg'>
+                <Button variant="primary" className='mx-2 py-3' onClick={goToMain} >Volver a la página de inicio</Button>
+                <Button variant="secondary" className='mx-2 py-3' onClick={goToLogin}>Ir al inicio de sesión</Button></ButtonGroup>
         </div>
     )
 }
